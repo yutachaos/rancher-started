11311e618afa2c41a028cbd95a731c284b4f007a
yutaChaos <y_kimura@r.recruit.co.jp>
2018-12-01 00:15:36 +0900

    Added resource

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -4,11 +4,29 @@ resource "tls_private_key" "node-key" {
 }
 
 locals {
+  machine_type = "n1-standard-1"
+  region       = "asia-northeast1"
+  zone         = "asia-northeast1-a"
   ssh_username = "ubuntu"
-  boot_script = <<EOT
+  image_name   = "ubuntu-os-cloud/ubuntu-1604-lts"
+
+  host_boot_script = <<EOT
 curl releases.rancher.com/install-docker/17.03.sh | bash
-sudo usermod -a -G docker ubuntu
-sudo echo ${tls_private_key.node-key.private_key_pem} > /${local.ssh_username}/.ssh/node_id_isa
+sudo usermod -a -G docker ${local.ssh_username}
+cat <<EOS > /home/${local.ssh_username}/.ssh/id_rsa
+${tls_private_key.node-key.private_key_pem}EOS
+chmod 600 /home/${local.ssh_username}/.ssh/id_rsa
+chown ${local.ssh_username}:${local.ssh_username} /home/${local.ssh_username}/.ssh/id_rsa
+sudo snap install kubectl --classic
+kubectl completion bash >> ~/.bash_profile
+wget https://github.com/rancher/rke/releases/download/v0.1.9/rke_linux-amd64
+chmod +x rke_linux-amd64
+sudo mv rke_linux-amd64 /usr/local/bin/rke
+EOT
+
+  node_boot_script = <<EOT
+curl releases.rancher.com/install-docker/17.03.sh | bash
+sudo usermod -a -G docker ${local.ssh_username}
 EOT
 }
 
@@ -23,7 +41,7 @@ resource "google_compute_subnetwork" "development" {
   ip_cidr_range = "10.30.0.0/16"
   network       = "${google_compute_network.network.name}"
   description   = "development"
-  region        = "asia-northeast1"
+  region        = "${local.region}"
 }
 
 # gcp_firewall.tf
@@ -35,9 +53,15 @@ resource "google_compute_firewall" "development" {
     protocol = "icmp"
   }
 
+  // @see https://rancher.com/docs/rancher/v2.x/en/installation/references/#rancher-nodes
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443"]
+    ports    = ["22", "80", "443", "2376", "2379", "2380", "6443", "8080", "9099", "10250", "10254", "30000-32767"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["30000-32767"]
   }
 
   source_ranges = ["0.0.0.0/0"]
@@ -46,13 +70,15 @@ resource "google_compute_firewall" "development" {
 
 resource "google_compute_instance" "rke-host" {
   name         = "host"
-  machine_type = "f1-micro"
-  zone         = "asia-northeast1-a"
+  machine_type = "${local.machine_type}"
+  zone         = "${local.zone}"
   tags         = ["development"]
+
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-lts"
+      image = "${local.image_name}"
     }
   }
 
@@ -64,25 +90,25 @@ resource "google_compute_instance" "rke-host" {
     subnetwork = "${google_compute_subnetwork.development.name}"
   }
 
-  metadata_startup_script = "${local.boot_script}"
+  metadata_startup_script = "${local.host_boot_script}"
 
   metadata {
     "block-project-ssh-keys" = "true"
     "ssh-keys"               = "${local.ssh_username}:${tls_private_key.node-key.public_key_openssh}"
   }
-
-  depends_on = ["google_compute_firewall.development"]
 }
 
 resource "google_compute_instance" "rke-node1" {
   name         = "node1"
-  machine_type = "f1-micro"
-  zone         = "asia-northeast1-a"
+  machine_type = "${local.machine_type}"
+  zone         = "${local.zone}"
   tags         = ["development"]
+
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-lts"
+      image = "${local.image_name}"
     }
   }
 
@@ -99,20 +125,20 @@ resource "google_compute_instance" "rke-node1" {
     "ssh-keys"               = "${local.ssh_username}:${tls_private_key.node-key.public_key_openssh}"
   }
 
-  metadata_startup_script = "${local.boot_script}"
-
-  depends_on = ["google_compute_firewall.development"]
+  metadata_startup_script = "${local.node_boot_script}"
 }
 
 resource "google_compute_instance" "rke-node2" {
   name         = "node2"
-  machine_type = "f1-micro"
-  zone         = "asia-northeast1-a"
+  machine_type = "${local.machine_type}"
+  zone         = "${local.zone}"
   tags         = ["development"]
+
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-lts"
+      image = "${local.image_name}"
     }
   }
 
@@ -123,23 +149,26 @@ resource "google_compute_instance" "rke-node2" {
 
     subnetwork = "${google_compute_subnetwork.development.name}"
   }
+
   metadata {
     "block-project-ssh-keys" = "true"
     "ssh-keys"               = "${local.ssh_username}:${tls_private_key.node-key.public_key_openssh}"
   }
-  metadata_startup_script = "${local.boot_script}"
-  depends_on = ["google_compute_firewall.development"]
+
+  metadata_startup_script = "${local.node_boot_script}"
 }
 
 resource "google_compute_instance" "rke-node3" {
   name         = "node3"
-  machine_type = "f1-micro"
-  zone         = "asia-northeast1-a"
+  machine_type = "${local.machine_type}"
+  zone         = "${local.zone}"
   tags         = ["development"]
+
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1604-lts"
+      image = "${local.image_name}"
     }
   }
 
@@ -156,7 +185,5 @@ resource "google_compute_instance" "rke-node3" {
     "ssh-keys"               = "${local.ssh_username}:${tls_private_key.node-key.public_key_openssh}"
   }
 
-  metadata_startup_script = "${local.boot_script}"
-
-  depends_on = ["google_compute_firewall.development"]
+  metadata_startup_script = "${local.node_boot_script}"
 }
