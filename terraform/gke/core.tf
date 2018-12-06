b8b91d8b661feb29fe294bc386024fa5aebef3ec
yutaChaos <y_kimura@r.recruit.co.jp>
2018-12-06 13:04:04 +0900

    Add gke resource

---
+++ terraform/gke/core.tf
@@ -0,0 +1,8 @@
+provider "google" {
+  credentials = "${file("account.json")}"
+  project     = "rancher-practice"
+  region      = "asia-northeast1"
+}
+
+variable "cluster_username" {}
+variable "cluster_password" {}
